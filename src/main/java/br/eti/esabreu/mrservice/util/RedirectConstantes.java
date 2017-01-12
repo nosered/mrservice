fec20cd4fd01f8d77065229d9909a2badbf39ede
Ederson Abreu <ederson.dimm@gmail.com>
2017-01-12 10:23:20 -0300

    desenvolvimento do frontend para o crud do cliente

--- src/main/java/br/eti/esabreu/mrservice/util/RedirectConstantes.java
+++ src/main/java/br/eti/esabreu/mrservice/util/RedirectConstantes.java
@@ -3,4 +3,5 @@ package br.eti.esabreu.mrservice.util;
 public class RedirectConstantes {
 	
 	public static final String REDIRECT_LISTAR_CLIENTES = "redirect:/cliente/listar";
+	public static final String REDIRECT_DETALHES_CLIENTE = "redirect:/cliente/detalhes/";
 }
