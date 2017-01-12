973b4a27034bfdab3e8f215e25c03296d7437673
Mariana <Mariana@192.168.2.13>
2017-01-11 21:04:24 -0300

    criado o crud de servico e class test

--- src/main/java/br/eti/esabreu/mrservice/util/RedirectConstantes.java
+++ src/main/java/br/eti/esabreu/mrservice/util/RedirectConstantes.java
@@ -3,4 +3,6 @@ package br.eti.esabreu.mrservice.util;
 public class RedirectConstantes {
 	
 	public static final String REDIRECT_LISTAR_CLIENTES = "redirect:/cliente/listar";
+	
+	public static final String REDIRECT_LISTAR_SERVICOS = "redirect:/servico/listar";
 }
